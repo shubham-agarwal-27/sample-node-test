161d8d5ebad433a4e6ab50897f3b3aecb2d6955d
shubham-agarwal-27 <f20170126@goa.bits-pilani.ac.in>
2020-06-12 10:08:42 +0530

    abc

---
+++ open.js
@@ -0,0 +1,17 @@
+// var openInEditor = require('open-in-editor');
+// var editor = openInEditor.configure({
+//     editor: 'code'
+// }, function(err){
+//     console.error('Something went wrong: ' + err);
+// });
+// editor.open('give_inputs.txt')
+// .then(function() {
+//     console.log('Success!');
+// }, function(err) {
+//     console.error('Something went wrong: ' + err);
+// });
+
+
+const launch = require('launch-editor')
+
+launch('workflow.yml');
